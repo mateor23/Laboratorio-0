a7d7612a9e3da80e281caf1bf0fae6968974d4b6
mateor23 <mateo.ricaurte@udea.edu.co>
2020-09-17 23:28:09 -0500

    ejercicio 1

---
+++ main.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    char letra;
+    for (int i = 0;i < 7; i++) {
+        letra ='A';
+        for (int j = 0;j <= i; j++) {
+            cout << letra << ' ';
+            letra = letra + 1;
+        }
+        for (int j = 0; j < 11-2*i; j++) {
+            cout << ". ";
+        }
+        letra = 'A' + i;
+        for ( int j = i; j>=0; j--) {
+            if (letra != 'G') cout << letra << ' ';
+            letra = letra - 1;
+        }
+        cout << '\n';
+    }
+    return 0;
+}
